64c4011a0700cb992f9d30e9a613b213d7bc3020
Abisola <23akinyemia@stroudhigh.gloucs.sch.uk>
2026-07-26 19:16:31 +0100

    Added PKA values and defined the calculate_net_charge function to calculate net charges
    Signed-off-by: Abisola <23akinyemia@stroudhigh.gloucs.sch.uk>

--- scripts/data_features.py
+++ scripts/data_features.py
@@ -16,3 +16,26 @@ BULKINESS_SCALE = {
     'L': 21.40, 'K': 15.71, 'M': 16.25, 'F': 19.80, 'P': 17.43,
     'S': 9.47, 'T': 15.77, 'W': 21.67, 'Y': 18.03, 'V': 21.57
 }
+
+#Standard pKa values for Isoelectric Point Calculation
+PKA_VALUES = {
+    'N_term': 9.69, 'C_term': 2.34,
+    'K': 10.53, 'R': 12.48, 'H': 6.00,  # Basic groups
+    'D': 3.86,  'E': 4.25,  'C': 8.33, 'Y': 10.07 # Acidic groups
+
+}
+
+# - Calculation functions -
+def calculate_net_charge(sequence, ph=7.4):
+    charge = 1.0/(1.0+10**(ph-PKA_VALUES['N-term']))
+    charge -= 1.0/(1.0+10**(ph-PKA_VALUES['C-term']))
+    #side chains
+    charge += (sequence.count('K') * 1.0/ (1.0 + 10** (ph-PKA_VALUES['K'])))
+    charge += (sequence.count('R') * 1.0 / (1.0 + 10 ** (ph - PKA_VALUES['R'])))
+    charge += (sequence.count('H') * 1.0 / (1.0 + 10 ** (ph - PKA_VALUES['H'])))
+
+    charge -= (sequence.count('D') * 1.0 / (1.0 + 10 ** (PKA_VALUES['D'] - ph)))
+    charge -= (sequence.count('E') * 1.0 / (1.0 + 10 ** (PKA_VALUES['E'] - ph)))
+    charge -= (sequence.count('C') * 1.0 / (1.0 + 10 ** (PKA_VALUES['C'] - ph)))
+    charge -= (sequence.count('Y') * 1.0 / (1.0 + 10 ** (PKA_VALUES['Y'] - ph)))
+    return float(charge)
